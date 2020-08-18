4d6bad4845e334fe24c943ac087e38c48d928f26
Ollie Nye <ollie@nyemail.co.uk>
2020-08-18 10:33:09 +0100

    Add AppProvider context globally

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { AppProvider } from './state/AppContext'
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
